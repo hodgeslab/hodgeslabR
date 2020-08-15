fcd88380e1f5f05af258fa3bcd982c166f588a57
Courtney Hodges <chodges@boltzmann.lan>
2020-08-15 14:42:57 -0500

    20200815_001

--- R/theme_hodgeslab_basic.R
+++ R/theme_hodgeslab_basic.R
@@ -15,10 +15,10 @@ theme_hodgeslab_basic <- function(base_size = 7, base_family = "", line_size = 0
 
   p <- theme_classic(base_size = base_size, base_family = base_family) %+replace%
     theme(
-      axis.text.x =       element_text(size = base_size * 1, lineheight = 1, vjust = 1, margin=margin(1,0,0,0)),
-      axis.text.y =       element_text(size = base_size * 1, lineheight = 1, hjust = 1, margin=margin(0,1,0,0)),
-      axis.title.x =      element_text(size = base_size, vjust = 1, margin=margin(base_size/2,0,0,0)),
-      axis.title.y =      element_text(size = base_size, angle = 90, vjust = 0, margin=margin(0,base_size/2,0,0)),
+      axis.text.x =       element_text(size = base_size * 1, lineheight = 1, vjust = 1, colour="black", margin=margin(1,0,0,0)),
+      axis.text.y =       element_text(size = base_size * 1, lineheight = 1, hjust = 1, colour="black", margin=margin(0,1,0,0)),
+      axis.title.x =      element_text(size = base_size, vjust = 1, colour="black", margin=margin(base_size/2,0,0,0)),
+      axis.title.y =      element_text(size = base_size, angle = 90, vjust = 0, colour="black", margin=margin(0,base_size/2,0,0)),
       axis.line.x =       element_line(size = line_size*linescale, linetype="solid", colour="black"),
       axis.line.y =       element_line(size = line_size*linescale, linetype="solid", colour="black"),
       axis.ticks =        element_line(size = line_size*linescale, colour = "black"),
